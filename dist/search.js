e598160136e94611ff928dc13f7478f9ad1897e5
Boris Cherny <bcherny@turn.com>
2014-06-02 11:27:04 -0700

    fix turn-icon-font dependency in bowerfile

---
+++ dist/search.js
@@ -0,0 +1,80 @@
+angular.module('turn/searchTemplate', ['search.html']);
+
+angular.module("search.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("search.html",
+    "<form class=\"search\">\n" +
+    "	<span class=\"tif-search\"></span>\n" +
+    "	<span class=\"tif-delete\" ng-show=\"param\" ng-click=\"clear()\"></span>\n" +
+    "	<input\n" +
+    "		type=\"search\"\n" +
+    "		ng-model=\"param\"\n" +
+    "		class=\"{{ class }}\"\n" +
+    "		ng-disabled=\"disabled\"\n" +
+    "		placeholder=\"{{ placeholder }}\"\n" +
+    "	>\n" +
+    "	<span\n" +
+    "		class=\"press-enter\"\n" +
+    "		ng-click=\"update()\"\n" +
+    "		ng-show=\"!typeAhead && dirty && !loading\"\n" +
+    "	>Press <kbd>enter</kbd> to search</span>\n" +
+    "</form>");
+}]);
+
+/**
+ * Search directive
+ * @description a nice text input for search
+ * @authors
+ *   Hanna Hoang <hhoang@turn.com>
+ *   Chris Zheng <czheng@turn.com>
+ *   Boris Cherny <bcherny@turn.com>
+ */
+angular.module('turn/search', ['turn/searchTemplate']).constant('SEARCH_KEYS', {
+  enter: 13,
+  esc: 27
+}).directive('search', [
+  'SEARCH_KEYS',
+  function (SEARCH_KEYS) {
+    return {
+      restrict: 'E',
+      scope: {
+        param: '=',
+        class: '@',
+        typeAhead: '=',
+        placeholder: '@',
+        disabled: '=',
+        search: '&'
+      },
+      templateUrl: 'search.html',
+      link: function (scope, element, attrs) {
+        angular.extend(scope, {
+          dirty: false,
+          blur: function () {
+            element.find('input').blur();
+          },
+          change: function (e) {
+            var code = e.keyCode || e.which;
+            if (code === SEARCH_KEYS.esc) {
+              scope.clear();
+              scope.blur();
+            } else if (code === SEARCH_KEYS.enter || scope.typeAhead) {
+              scope.update();
+            } else {
+              scope.dirty = true;
+            }
+          },
+          update: function () {
+            scope.search({ $param: scope.param });
+            scope.dirty = false;
+          },
+          clear: function () {
+            scope.param = '';
+            scope.update();
+          }
+        });
+        // TODO: investigate why this doesn't work when
+        //       declared in the DOM
+        element.find('input').on('keyup paste', scope.change);
+      }
+    };
+  }
+]);
---
+++ src/search.js
@@ -0,0 +1,91 @@
+/**
+ * Search directive
+ * @description a nice text input for search
+ * @authors
+ *   Hanna Hoang <hhoang@turn.com>
+ *   Chris Zheng <czheng@turn.com>
+ *   Boris Cherny <bcherny@turn.com>
+ */
+
+angular
+.module('turn/search', ['turn/searchTemplate'])
+.constant('SEARCH_KEYS', {
+	enter: 13,
+	esc: 27
+})
+.directive('search', function (SEARCH_KEYS) {
+
+	return {
+		restrict: 'E',
+		scope: {
+			param: '=',
+			class: '@',
+			typeAhead: '=',
+			placeholder: '@',
+            disabled: '=',
+            search: '&'
+		},
+		templateUrl: 'search.html',
+		link: function (scope, element, attrs) {
+
+			angular.extend(scope, {
+
+				/**
+				 * True when the user has entered text that
+				 * has not yet been submitted
+				 * @type {Boolean}
+				 */
+				dirty: false,
+
+				/**
+				 * Programatically blurs the <input>
+				 */
+				blur: function() {
+					element.find('input').blur();
+				},
+
+				/**
+				 * Fired by the <input> on onKeyUp
+				 * @param  {Event} e
+				 */
+				change: function(e) {
+					var code = e.keyCode || e.which;
+
+					if (code === SEARCH_KEYS.esc) {
+						scope.clear();
+						scope.blur();
+					} else if (code === SEARCH_KEYS.enter || scope.typeAhead) {
+						scope.update();
+					} else {
+						scope.dirty = true;
+					}
+				},
+
+				/**
+				 * Updates the model that the user passed in (attributes#param)
+				 */
+				update: function() {
+					scope.search({
+						$param: scope.param
+					});
+					scope.dirty = false;
+				},
+
+				/**
+				 * Clears the <input>
+				 */
+				clear: function() {
+					scope.param = '';
+					scope.update();
+				}
+
+			});
+
+			// TODO: investigate why this doesn't work when
+			//       declared in the DOM
+			element.find('input').on('keyup paste', scope.change);
+
+		}
+	};
+
+});
